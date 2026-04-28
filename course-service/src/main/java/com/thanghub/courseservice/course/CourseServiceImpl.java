2d062b947a84e4875c4426e3d67a2a1f68735582
thangdtgroup <thangdt@vmogroup.com>
2026-04-28 16:56:21 +0700

    Create course api

--- course-service/src/main/java/com/thanghub/courseservice/course/CourseServiceImpl.java
+++ course-service/src/main/java/com/thanghub/courseservice/course/CourseServiceImpl.java
@@ -1,7 +1,10 @@
 package com.thanghub.courseservice.course;
 
+import com.thanghub.common.enums.CourseStatusEnum;
 import com.thanghub.courseservice.course.Course;
 import com.thanghub.courseservice.course.CourseRepository;
+import com.thanghub.courseservice.course.request.CreateCourseRequestDto;
+import com.thanghub.courseservice.course.request.UpdateCourseRequestDto;
 import com.thanghub.courseservice.course.response.CourseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,6 +20,31 @@ public class CourseServiceImpl implements CourseService {
     public Page<CourseResponse> getCourses(Pageable pageable) {
         return courseRepository.findAll(pageable)
                 .map(this::toResponse);
+    }
+
+    @Override
+    public Course createCourse(CreateCourseRequestDto request) {
+        return courseRepository.save(convertCourseDtoToEntity(request));
+    }
+
+    @Override
+    public Course updateCourse(String id, UpdateCourseRequestDto request) {
+        // Find by ID
+        Course course = courseRepository.findById(id).orElseThrow(() -> new RuntimeException("Course not found"));
+        course.setTitle(request.getTitle());
+        course.setDescription(request.getDescription());
+        course.setLevel(request.getLevel());
+        return courseRepository.save(course);
+    }
+
+    private Course convertCourseDtoToEntity(CreateCourseRequestDto request) {
+        return Course
+                .builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .level(request.getLevel())
+                .status(CourseStatusEnum.DRAFT)
+                .build();
     }
 
     private CourseResponse toResponse(Course course) {
